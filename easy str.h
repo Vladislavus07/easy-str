e2581e16fa5cfb0cfac64fcc907e23ac9783d83f
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-10-02 10:01:34 +0300

    Add files via upload

---
+++ easy str.h
@@ -0,0 +1,10 @@
+#ifndef EASY_STR_H_INCLUDED
+#define EASY_STR_H_INCLUDED
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string itc_hello_str(string name);
+
+#endif // EASY_STR_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "easy str.h"
+using namespace std;
+
+string itc_hello_str(string name)
+ {
+	cin >> name;
+	return "Hello, " + name;
+}
+
+int main() {
+    string name;
+    //cin >> name;
+    cout << itc_hello_str(name);
+    return 0;
+}
