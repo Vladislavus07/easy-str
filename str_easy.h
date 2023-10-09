0dccaeb6a7e4e0f74cf26caae8f2d68ac1db0f26
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-10-09 11:07:11 +0300

    Update str_easy.h

--- str_easy.h
+++ str_easy.h
@@ -8,6 +8,6 @@ string itc_hello_str(string name);
 long long itc_len(string str);
 void itc_print_copy_str(string str, int number);
 void  itc_first_end_three(string str);
-int itc_cout_char_in_str(char ch, string str);
+int itc_count_char_in_str(char ch, string str);
 
 #endif // EASY_STR_H_INCLUDED
